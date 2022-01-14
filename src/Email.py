e04b44ee6b3aac8373d76b00ae638c32d9a9d64e
jakeiridion <emil.ej.jeleskovic@gmail.com>
2022-01-14 15:57:26 +0100

    changed the data of prev.txt file and fixed crash when no emails are listed in receivers.txt.

--- src/Email.py
+++ src/Email.py
@@ -1,23 +1,29 @@
 import smtplib
 import ssl
 import re
+import sys
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import os
 from Crawler import Crawler
+from Logger import setup_logger
 
 
 class Email:
     def __init__(self):
+        self._log = setup_logger(__name__)
         self._smtp_server = os.getenv("SMTP_SERVER")
         self._smtp_server_port = 465
         self._username = os.getenv("EMAIL_ADDRESS")
         self._password = os.getenv("EMAIL_PASSWORD")
         self._receivers = self._get_receivers()
+        if not self._receivers:
+            self._log.info("No Emails listed in receivers.txt")
+            sys.exit(0)
 
     def _get_receivers(self):
         with open("receivers.txt", "r") as file:
-            return [line.strip() for line in file.readlines()]
+            return [line.strip() for line in file.readlines() if not line.startswith("#")]
 
     def send_mail(self, soup):
         context = ssl.create_default_context()
--- src/main.py
+++ src/main.py
@@ -15,25 +15,26 @@ class App:
     def run(self):
         self._log.info("Starting App...")
         soup = self._crawler.get_html_table()
-        if self._do_send(soup):
+        table = soup.find_all("table")
+        if self._do_send(str(table)):
             self._log.info("Sending Mail")
             self._email.send_mail(soup)
             self._log.info("Updating Previous Event Date.")
             with open("prev.txt", "w") as file:
-                file.write(Crawler.get_current_event_date_str(soup))
+                file.write(str(table))
         self._log.info("No new Events!")
         self._log.info("Done!")
 
-    def _do_send(self, soup):
+    def _do_send(self, table):
         self._log.info("Checking Events.")
-        no_event_count = str(soup).count("Keine Vertretungen")
+        no_event_count = table.count("Keine Vertretungen")
         self._log.info(f"No Event Count: {no_event_count}")
         if no_event_count == 2:
             return False
-        return not Crawler.get_current_event_date_str(soup) == self._get_previous_event_date_str()
+        return not table == self._get_previous_data()
 
-    def _get_previous_event_date_str(self):
-        self._log.info("Retrieving previous event date info.")
+    def _get_previous_data(self):
+        self._log.info("Retrieving previous date.")
         if os.path.isfile("./prev.txt"):
             with open("prev.txt", "r") as file:
                 return file.read().strip()
